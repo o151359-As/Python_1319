ed65230eae7398a6f072571693d64acf4dcae2a6
Nikhil Patil <o151359as@gmail.com>
2026-02-10 21:47:20 +0530

    Create feb10.py

---
+++ feb10.py
@@ -0,0 +1,22 @@
+# decimal to (binary,oct,hex)
+# print("Enter decimal number")
+# dec=int(input("decimal:"))
+# print("binary is:",bin(dec))
+# print("octal is:",oct(dec))
+# print("hexadecimal is:",hex(dec))
+
+#(binary,oct,hex) to decimal
+# print("enter binary")
+# binary=(input("binary:"))
+# decimal= int(binary,2)
+# print("decimal is:",decimal)
+
+# print("enter octal")
+# octal=(input("octal:"))
+# decimal= int(octal,8)
+# print("decimal is:",decimal)
+
+# print("enter hexadecimal")
+# hex=(input("hex:"))
+# decimal= int(hex,16)
+# print("decimal is:",decimal)

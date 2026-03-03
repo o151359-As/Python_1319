5b58c08d9b3224caa79ca56a38e640d0ce3fa603
Nikhil Patil <o151359as@gmail.com>
2026-03-03 09:49:37 +0530

    Create mar2.py

---
+++ mar2.py
@@ -0,0 +1,25 @@
+s="instagram"
+for i in range(len("instagram")):
+    print(i,"--->",s[i])
+     
+s="instagram"
+for i in range(8,-1,-1):
+    print(i,"--->",s[i])
+
+
+l=[8,7,3,4,5]
+for i in range(len(l)):
+    print(i,"--->",l[i])
+
+l=[8,7,3,4,5]
+for i in range(4,-1,-1):
+    print(i,"--->",l[i])
+
+
+t=(8,7,3,4,5)
+for i in range(len(t)):
+    print(i,"--->",t[i])
+
+t=(8,7,3,4,5)
+for i in range(4,-1,-1):
+    print(i,"--->",t[i])
